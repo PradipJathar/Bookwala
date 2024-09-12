1bfed62742155f100d099243fc92df34ebbc87cc
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-09-12 12:46:43 +0530

    011 Get all Categories

--- BookwalaWeb/Controllers/CategoryController.cs
+++ BookwalaWeb/Controllers/CategoryController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookwalaWeb.Data;
+using BookwalaWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookwalaWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext db;
+        public CategoryController(ApplicationDbContext context)
+        {
+            db = context;
+        }
+
         public IActionResult Index()
         {
+            List<Category> categories = db.Categories.ToList();
+
             return View();
         }
     }
