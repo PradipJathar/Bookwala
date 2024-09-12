548177b2c5f9004c6ac8597c382b130be0636b10
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-09-12 15:51:43 +0530

    022 Client Side Validation

--- BookwalaWeb/Controllers/CategoryController.cs
+++ BookwalaWeb/Controllers/CategoryController.cs
@@ -27,10 +27,10 @@ namespace BookwalaWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category category)
         {
-            //if (category.Name == category.DisplayOrder.ToString())
-            //{
-            //    ModelState.AddModelError("Name", "Display Order cannot exactly match the Name.");
-            //}
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Display Order cannot exactly match the Name.");
+            }
 
             if (ModelState.IsValid)
             {
