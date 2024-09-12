a810b717d636cefbb354d0cd87ba114cccf2ea31
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-09-12 12:21:09 +0530

    007 Create Category Table

--- BookwalaWeb/Data/ApplicationDbContext.cs
+++ BookwalaWeb/Data/ApplicationDbContext.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BookwalaWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookwalaWeb.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {            
+        {
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
