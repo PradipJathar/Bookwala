bed28fa16a6b40a1d0bda13e2a2614984cc94a4b
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-09-12 16:08:11 +0530

    024 Get Category Details to Edit

--- BookwalaWeb/Controllers/CategoryController.cs
+++ BookwalaWeb/Controllers/CategoryController.cs
@@ -42,5 +42,22 @@ namespace BookwalaWeb.Controllers
 
             return View();
         }
+        
+        public IActionResult Edit(int? id) 
+        { 
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = db.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
     }
 }
