c77e06d97bbc238631cbdcb1b77b87c903511f48
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-09-13 12:49:18 +0530

    005 Display all Categories

--- BookwalaWebRazor_Temp/Pages/Categories/Index.cshtml.cs
+++ BookwalaWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using BookwalaWebRazor_Temp.Data;
+using BookwalaWebRazor_Temp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,18 @@ namespace BookwalaWebRazor_Temp.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+        ApplicationDbContext db;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            db = context;
+        }
+
+        public List<Category> CategoryList { get; set; }
+
         public void OnGet()
         {
+            CategoryList = db.Categories.ToList();
         }
     }
 }
