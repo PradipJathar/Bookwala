4d963776f7bd342de844f83d8b972e49bf1bb144
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-09-13 12:30:51 +0530

    003 Setup EF Core

---
+++ BookwalaWebRazor_Temp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookwalaWebRazor_Temp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookwalaWebRazor_Temp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
