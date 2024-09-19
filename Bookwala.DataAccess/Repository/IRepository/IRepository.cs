27ef6a6f1ea93aeadc5010db1bd507fa7310c731
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-09-19 14:56:25 +0530

    003 Implement ICategoryRepository

--- Bookwala.DataAccess/Repository/IRepository/IRepository.cs
+++ Bookwala.DataAccess/Repository/IRepository/IRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Bookwala.DataAccess.Repository.IRepository
 {
-    internal interface IRepository<T> where T : class
+    public interface IRepository<T> where T : class
     {
         IEnumerable<T> GetAll();
 
