8b01d9dee69b454e3ebdf2f22939c06e8060ee21
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-09-19 13:11:45 +0530

    002 Implement Repository Interface

---
+++ Bookwala.DataAccess/Repository/IRepository/Repository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bookwala.DataAccess.Data;
+using Bookwala.DataAccess.Repository.IRepository;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookwala.DataAccess.Repository.IRepository
+{
+    internal class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            db = context;
+            this.dbSet = db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(Func<T, bool> filer)
+        {
+            IQueryable<T> query = dbSet;
+            query = (IQueryable<T>)query.Where(filer);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
