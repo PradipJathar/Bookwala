8694f6b9e589e6bd6bb95d8c929a93730e356204
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-09-23 12:42:13 +0530

    003 Assignment 1 Solution - Product Repository and UnitOfWork

---
+++ Bookwala.DataAccess/Repository/IRepository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Bookwala.DataAccess.Data;
+using Bookwala.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookwala.DataAccess.Repository.IRepository
+{
+    internal class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext db;
+
+        public ProductRepository(ApplicationDbContext context) : base(context)
+        {
+            db = context;
+        }
+
+        public void Update(Product product)
+        {
+            db.Products.Update(product);
+        }
+    }
+}
--- Bookwala.DataAccess/Repository/IRepository/UnitOfWork.cs
+++ Bookwala.DataAccess/Repository/IRepository/UnitOfWork.cs
@@ -8,19 +8,24 @@ using System.Threading.Tasks;
 namespace Bookwala.DataAccess.Repository.IRepository
 {
     public class UnitOfWork : IUnitOfWork
-    {
-        public ICategoryRepository Category { get; private set; }
-
+    {       
         private readonly ApplicationDbContext db;
+
         public UnitOfWork(ApplicationDbContext context)
         {
             db = context;
             this.Category = new CategoryRepository(db);
+            this.Product = new ProductRepository(db);
         }
 
         public void Save()
         {
             db.SaveChanges();
         }
+
+
+        public ICategoryRepository Category { get; private set; }
+        public IProductRepository Product { get; private set; }
+        
     }
 }
