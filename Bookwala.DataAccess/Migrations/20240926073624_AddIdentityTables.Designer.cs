177f6ab57187030347abc6c404bc08f529474574
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-09-26 13:25:51 +0530

    004 Add Identity Tables

--- Bookwala.DataAccess/Migrations/20240926073624_AddIdentityTables.Designer.cs
+++ Bookwala.DataAccess/Migrations/20240926073624_AddIdentityTables.Designer.cs
@@ -4,6 +4,7 @@ using Bookwala.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bookwala.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240926073624_AddIdentityTables")]
+    partial class AddIdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
