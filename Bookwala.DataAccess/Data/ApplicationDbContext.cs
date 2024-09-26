aa7124ac03a6cd8bdea4bdecfc868a766cd97b29
Pradip <Pradip@DESKTOP-7D2H6S8>
2024-09-26 12:39:53 +0530

    002 Scaffold Identity Issue (NET8)

--- Bookwala.DataAccess/Data/ApplicationDbContext.cs
+++ Bookwala.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using Bookwala.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookwala.DataAccess.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
